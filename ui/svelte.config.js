1f7e13981c2726f39d50e7a6aaa5528a50036526
~nallux-dozryl <nallux-dozryl@protonmail.com>
2022-08-25 20:16:50 +0800

    minor fixes

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,14 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+    prerender: {
+      default: true
+    },
+		adapter: adapter({
+      fallback: null
+    })
 	}
 };
 
