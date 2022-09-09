621f425656bc3c41ff79f8a6faeea19c641c310f
nallux-dozryl <nallux-dozryl@protonmail.com>
2022-09-10 07:04:16 +0800

    fixed nodejs and docker install

--- build.sh
+++ build.sh
@@ -12,11 +12,21 @@ else
     fi
 fi
 
-# Install Requirements
-sudo pip3 install -r requirements.txt
+# Install Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-sudo apt-get -y install npm wireguard docker nodejs
-sudo npm install -g node
+# Get latest Nodejs
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+# Install Requirements
+sudo apt-get -y install wireguard nodejs python3-pip
+sudo pip3 install -r requirements.txt
 
 
 # Make build directory
