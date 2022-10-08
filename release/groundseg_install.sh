3117f54faad252cd15e6526ebe4bb50e202570e1
nallux-dozryl <nallux-dozryl@protonmail.com>
2022-10-08 08:25:43 +0800

    fixed typo

--- release/groundseg_install.sh
+++ release/groundseg_install.sh
@@ -2,7 +2,7 @@ sudo mkdir -p /opt/nativeplanet/groundseg/
 wget -O /opt/nativeplanet/groundseg/docker-compose.yml \
 	https://github.com/nallux-dozryl/GroundSeg/main/release/docker-compose.yml
 
-sudo wget -O /etc/systemd/system/ \
+sudo wget -O /etc/systemd/system/groundseg.service \
 https://raw.githubusercontent.com/nallux-dozryl/GroundSeg/main/release/groundseg.sevice
 sudo systemctl stop groundseg
 sudo systemctl enable groundseg
