e4cc6a1eaa1b506b3ec1dbb47d633f0542252a96
nallux-dozryl <nallux-dozryl@protonmail.com>
2022-10-12 06:49:19 +0800

    fixed docker compose command not found bug

--- release/groundseg_install.sh
+++ release/groundseg_install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-export HOST_HOSTNAME=$(hostname)
 sudo mkdir -p /opt/nativeplanet/groundseg/
 wget -O /opt/nativeplanet/groundseg/docker-compose.yml \
 	https://raw.githubusercontent.com/nallux-dozryl/GroundSeg/main/release/docker-compose.yml
-wget -O /opt/nativeplanet/groundseg/groundseg.sh \
-	https://raw.githubusercontent.com/nallux-dozryl/GroundSeg/main/release/groundseg.sh
-
-sudo chmod +x /opt/nativeplanet/groundseg/groundseg.sh
 
 sudo wget -O /etc/systemd/system/groundseg.service \
 	https://raw.githubusercontent.com/nallux-dozryl/GroundSeg/main/release/groundseg.service
