386958278a9f897ac4028aeb87ecb2479dc08459
nallux-dozryl <nallux-dozryl@protonmail.com>
2022-10-12 14:28:22 +0800

    start docker service before installing groundseg

--- release/standard_install.sh
+++ release/standard_install.sh
@@ -15,6 +15,9 @@ sudo wget -O /usr/local/lib/docker/cli-plugins/docker-compose \
 	https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-linux-x86_64
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
+systemctl enable docker
+systemctl start docker
+
 wget -O /tmp/nativeplanet/groundseg_install.sh \
 	https://raw.githubusercontent.com/$ACC/$REPO/$BRANCH/release/groundseg_install.sh
 
