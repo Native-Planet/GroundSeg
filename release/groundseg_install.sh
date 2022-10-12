f243eb206b4e002387fe978f957efb43bfbcea9b
nallux-dozryl <nallux-dozryl@protonmail.com>
2022-10-13 05:32:47 +0800

    script for pipe

--- release/groundseg_install.sh
+++ release/groundseg_install.sh
@@ -13,11 +13,19 @@ sudo mkdir -p $SAVE_DIR
 wget -O $SAVE_DIR/docker-compose.yml \
 	https://raw.githubusercontent.com/$ACC/$REPO/$BRANCH/release/docker-compose.yml
 
+# Pipe for running commands on host
+wget -O $SAVE_DIR/opencmd.sh \
+	https://raw.githubusercontent.com/$ACC/$REPO/$BRANCH/release/opencmd.sh
+chmod +x $SAVE_DIR/opencmd.sh
+
 # Create pipe file
 sudo mkfifo $SAVE_DIR/commands
 
+# systemd unit
 sudo wget -O /etc/systemd/system/groundseg.service \
 	https://raw.githubusercontent.com/$ACC/$REPO/$BRANCH/release/groundseg.service
+
+# Load and start
 sudo systemctl enable groundseg
 sudo systemctl daemon-reload
 sudo systemctl restart groundseg
