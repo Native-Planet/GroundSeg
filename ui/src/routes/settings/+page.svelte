6d5f790841f1656b8afbcc136ca5bcf771e95a69
nallux-dozryl <nallux-dozryl@protonmail.com>
2022-11-08 06:42:46 +0800

    removed unused component

--- ui/src/routes/settings/+page.svelte
+++ ui/src/routes/settings/+page.svelte
@@ -17,8 +17,6 @@
   import MinIO from '$lib/MinIO.svelte'
   import Contact from '$lib/Contact.svelte'
 
-  import AutoPierStart from '$lib/AutoPierStart.svelte'
-
 	// load data into store
 	export let data
 	updateState(data)
