3c876a3f926c30afffd98c480023b3aa0b4a44f6
nallux-dozryl <nallux-dozryl@protonmail.com>
2022-11-29 09:31:31 +0800

    beta 3.3.0

--- build.sh
+++ build.sh
@@ -3,15 +3,14 @@ sudo docker stop $(sudo docker ps -aq)
 sudo docker rm -f $(sudo docker ps -aq)
 sudo docker volume prune -f
 sudo docker rmi -f $(sudo docker images -q)
-# sudo rm -r /opt/nativeplanet/groundseg/*
+sudo rm -r /opt/nativeplanet/groundseg/*
 
 # build binary
 python -m nuitka --clang --onefile api/groundseg.py -o groundseg
 
 # move binary
-sudo rm /opt/nativeplanet/groundseg/groundseg
 sudo mv groundseg /opt/nativeplanet/groundseg/groundseg
 
 # restart service
 sudo chmod +x /opt/nativeplanet/groundseg/groundseg
-#sudo systemctl restart groundseg
+sudo systemctl restart groundseg
