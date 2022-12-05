6c399c8d73920ede43ccc4aaa6cc2b6639786a5b
nallux-dozryl <nallux-dozryl@protonmail.com>
2022-12-06 07:43:47 +0800

    stable branch hash

--- api/orchestrator.py
+++ api/orchestrator.py
@@ -39,7 +39,7 @@ class Orchestrator:
     _disk = None
 
     # GroundSeg
-    gs_version = 'Beta-3.3.5-edge'
+    gs_version = 'Beta-3.3.5'
     anchor_config = {'lease': None,'ongoing': None}
     minIO_on = False
     config = {}
