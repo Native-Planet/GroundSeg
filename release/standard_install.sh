595434c0af4613c0de2801b0df9e865416c4fa4d
nallux-dozryl <nallux-dozryl@protonmail.com>
2022-12-08 06:54:43 +0800

    removed docker-compose from linuxmint install

--- release/standard_install.sh
+++ release/standard_install.sh
@@ -17,7 +17,7 @@ then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
-    sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo apt install docker-ce docker-ce-cli containerd.io
 else
     wget -O /tmp/nativeplanet/docker_install.sh https://get.docker.com/
     chmod +x /tmp/nativeplanet/docker_install.sh
