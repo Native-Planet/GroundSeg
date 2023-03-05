77bde6b66e0b38ff6330477fc152263329cc2246
nallux-dozryl <nallux-dozryl@protonmail.com>
2023-03-06 06:03:26 +0800

    improved system monitor

--- api/system_monitor.py
+++ api/system_monitor.py
@@ -8,46 +8,65 @@ class SysMonitor:
     def __init__(self, config):
         self.config_object = config
         self.config = config.config
+        self.mode = config.device_mode
 
-    def sys_monitor(self):
-        Log.log("Monitor: System monitor thread started")
-        error = False
+    # RAM info
+    def ram_monitor(self):
+        Log.log("Monitor: RAM monitor thread started")
         error_time = 15
-        while not self.config_object.device_mode == "vm":
-            if error:
-                Log.log(f"Monitor: System monitor error, Checking again in {error_time} seconds")
-                sleep(error_time)
-                error_time = error_time * 2
-                error = False
-
-            # RAM info
+        while not self.mode == "vm":
             try:
                 self.config_object._ram = psutil.virtual_memory().percent
+                sleep(1)
+                error_time = 15
             except Exception as e:
                 self.config_object._ram = 0.0
                 Log.log(f"Monitor: RAM info error: {e}")
-                error = True
+                Log.log(f"Monitor: Checking RAM info again in {error_time} seconds")
+                sleep(error_time)
+                error_time = error_time * 2
 
-            # CPU info
+    # CPU info
+    def cpu_monitor(self):
+        Log.log("Monitor: CPU monitor thread started")
+        error_time = 15
+        while not self.mode == "vm":
             try:
                 self.config_object._cpu = psutil.cpu_percent(1)
+                error_time = 15
             except Exception as e:
                 self.config_object._cpu = 0.0
                 Log.log(f"Monitor: CPU info error: {e}")
-                error = True
+                Log.log(f"Monitor: Checking CPU info again in {error_time} seconds")
+                sleep(error_time)
+                error_time = error_time * 2
 
-            # CPU Temp info
+    # CPU Temp info
+    def temp_monitor(self):
+        Log.log("Monitor: Core temperature monitor thread started")
+        error_time = 15
+        while not self.mode == "vm":
             try:
                 self.config_object._core_temp = psutil.sensors_temperatures()['coretemp'][0].current
+                error_time = 15
             except Exception as e:
                 self.config_object._core_temp = 0.0
-                Log.log(f"Monitor: Core Temp info error: {e}")
-                error = True
+                Log.log(f"Monitor: Core temperature info error: {e}")
+                Log.log(f"Monitor: Checking core temperature again in {error_time} seconds")
+                sleep(error_time)
+                error_time = error_time * 2
 
-            # Disk info
+    # Disk info
+    def disk_monitor(self):
+        Log.log("Monitor: Disk monitor thread started")
+        error_time = 15
+        while not self.mode == "vm":
             try:
                 self.config_object._disk = shutil.disk_usage("/")
+                error_time = 15
             except Exception as e:
                 self.config_object._disk = [0,0,0]
                 Log.log(f"Monitor: Disk info error: {e}")
-                error = True
+                Log.log(f"Monitor: Checking disk info again in {error_time} seconds")
+                sleep(error_time)
+                error_time = error_time * 2
