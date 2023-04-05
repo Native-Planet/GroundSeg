d5d821133f7e613510596937780e958f7092fbf4
nallux-dozryl <nallux-dozryl@protonmail.com>
2023-04-05 11:00:48 +0800

    added error handling

--- api/linux_updater.py
+++ api/linux_updater.py
@@ -12,30 +12,33 @@ class LinuxUpdater:
     def updater_loop(self):
         Log.log("Updater: Linux updater thread started")
         while True:
-            Log.log("Updater: Checking for linux updates")
-            if self.debug_mode:
-                # Fake values
-                upgrade, new, remove, ignore = [5,0,0,0]
-            else:
-                # Default values
-                upgrade, new, remove, ignore = [0,0,0,0]
+            try:
+                Log.log("Updater: Checking for linux updates")
+                if self.debug_mode:
+                    # Fake values
+                    upgrade, new, remove, ignore = [5,0,0,0]
+                else:
+                    # Default values
+                    upgrade, new, remove, ignore = [0,0,0,0]
 
-                # Update package list
-                subprocess.run(['apt','update'], shell=True)
+                    # Update package list
+                    subprocess.run(['apt','update'], shell=True)
 
-                # Simulate upgrade
-                output = subprocess.check_output(['apt','upgrade','-s'], shell=True).decode('utf-8').strip().split('\n')[-1]
+                    # Simulate upgrade
+                    output = subprocess.check_output(['apt','upgrade','-s'], shell=True).decode('utf-8').strip().split('\n')[-1]
 
-                # Regex
-                pattern = r"(\d+) upgraded, (\d+) newly installed, (\d+) to remove and (\d+) not upgraded."
-                updates = re.match(pattern, output)
-                if updates:
-                    upgrade, new, remove, ignore = map(int, updates.groups())
+                    # Regex
+                    pattern = r"(\d+) upgraded, (\d+) newly installed, (\d+) to remove and (\d+) not upgraded."
+                    updates = re.match(pattern, output)
+                    if updates:
+                        upgrade, new, remove, ignore = map(int, updates.groups())
 
-            # Set update notification
-            self.config_object.linux_updates = (upgrade + new + remove) > 0
+                # Set update notification
+                self.config_object.linux_updates = (upgrade + new + remove) > 0
 
-            Log.log(f"Updater: Linux updates: {upgrade} to upgrade, {new} to install, {remove} to remove, {ignore} to ignore")
+                Log.log(f"Updater: Linux updates: {upgrade} to upgrade, {new} to install, {remove} to remove, {ignore} to ignore")
+            except Exception as e:
+                Log.log(f"Updater: Failed to check for linux updates: {e}")
 
             # Check every 6 hours
             sleep(60 * 60 * 6)
