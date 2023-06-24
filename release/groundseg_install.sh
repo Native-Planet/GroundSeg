5c57262d9ed813afcb5b9a539462717909de21c3
nallux-dozryl <nallux-dozryl@protonmail.com>
2023-06-24 08:45:13 +0800

    version bump

--- release/groundseg_install.sh
+++ release/groundseg_install.sh
@@ -8,7 +8,7 @@ sudo firewall-cmd --reload
 ACC=Native-Planet
 REPO=GroundSeg
 BRANCH=master
-TAG=rc1-v1.3.5
+TAG=v1.4.0
 DEVICE_ARCH=$(uname -m)
 
 # Directory to save the scrips
--- ui/src/lib/api.js
+++ ui/src/lib/api.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store'
-export const webuiVersion = 'v1.3.5'
+export const webuiVersion = 'v1.4.0'
 
 //
 // fade transition params
