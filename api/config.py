ac5a19cd4135633cca22edf25bca594d8f492172
nallux-dozryl <nallux-dozryl@protonmail.com>
2023-07-03 20:42:37 +0800

    version bump to v1.4.1

--- api/config.py
+++ api/config.py
@@ -30,7 +30,7 @@ class Config:
     _arch = ""
 
     # Current version
-    version = "v1.4.0"
+    version = "v1.4.1"
 
     # Debug mode
     debug_mode = False
--- ui/src/lib/api.js
+++ ui/src/lib/api.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store'
-export const webuiVersion = 'v1.4.0'
+export const webuiVersion = 'v1.4.1'
 
 //
 // fade transition params
