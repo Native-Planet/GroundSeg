cbac210ea2e87e1f456e5d0ddb411e8e68aace9d
reid <reid@nativeplanet.io>
2023-09-04 14:09:08 -0500

    test simplified broadcast updates

--- goseg/rectify/rectify.go
+++ goseg/rectify/rectify.go
@@ -94,18 +94,9 @@ func NewShipTransitionHandler() {
 		event := <-docker.NewShipTransBus
 		switch event.Type {
 		case "error":
-			var res map[string]interface{}
-			res = map[string]interface{}{
-				"NewShip": map[string]interface{}{
-					"Transition": map[string]interface{}{
-						"Error": event.Event,
-					},
-				},
-			}
-			err := broadcast.UpdateBroadcastState(res)
-			if err != nil {
-				config.Logger.Warn(fmt.Sprintf("Error updating new ship transition 'error': %v", err))
-			}
+			current := broadcast.GetState()
+			current.NewShip.Transition.Error = event.Event
+			broadcast.UpdateBroadcast(current)
 			broadcast.BroadcastToClients()
 		case "bootStage":
 			// Events
@@ -115,32 +106,14 @@ func NewShipTransitionHandler() {
 			// completed: ready to reset
 			// aborted: something went wrong and we ran the cleanup routine
 			// <empty>: free for new ship
-			var res map[string]interface{}
-			res = map[string]interface{}{
-				"NewShip": map[string]interface{}{
-					"Transition": map[string]interface{}{
-						"BootStage": event.Event,
-					},
-				},
-			}
-			err := broadcast.UpdateBroadcastState(res)
-			if err != nil {
-				config.Logger.Warn(fmt.Sprintf("Error updating new ship transition 'bootStage': %v", err))
-			}
+			current := broadcast.GetState()
+			current.NewShip.Transition.BootStage = event.Event
+			broadcast.UpdateBroadcast(current)
 			broadcast.BroadcastToClients()
 		case "patp":
-			var res map[string]interface{}
-			res = map[string]interface{}{
-				"NewShip": map[string]interface{}{
-					"Transition": map[string]interface{}{
-						"Patp": event.Event,
-					},
-				},
-			}
-			err := broadcast.UpdateBroadcastState(res)
-			if err != nil {
-				config.Logger.Warn(fmt.Sprintf("Error updating new ship transition 'bootStage': %v", err))
-			}
+			current := broadcast.GetState()
+			current.NewShip.Transition.Patp = event.Event
+			broadcast.UpdateBroadcast(current)
 			broadcast.BroadcastToClients()
 		default:
 			config.Logger.Warn(fmt.Sprintf("Urecognized transition: %v", event.Type))
