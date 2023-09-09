f9dbdcc3ec4ca8dfe028e2ac4fa69dd4eaa279e5
nallux-dozryl <nallux-dozryl@protonmail.com>
2023-09-10 00:56:00 +0800

    load a ship styling

--- ui/src/routes/boot/+page.svelte
+++ ui/src/routes/boot/+page.svelte
@@ -8,18 +8,11 @@
 
 <div class="card-wrapper {wide ? "wide" : "slim"}">
   <div class="title">LOAD A SHIP</div>
-  <Option type="new" />
-  <Option type="existing"/>
+    <Option type="new" />
+    <Option type="existing"/>
 </div>
 
 <style>
-  .wide {
-    width: calc((288px * 3) + (80px * 2));
-    max-width: 100vw;
-  }
-  .slim {
-    width: calc(100vw - 40px);
-  }
   .card-wrapper {
     background: var(--bg-base);
     border-radius: 16px;
@@ -27,13 +20,27 @@
     height: 70vh;
     padding: 40px;
     display:flex;
-    gap: 40px;
     flex-direction: column;
     align-items: center;
+    display: flex;
+    width: calc(1104px - 80px);
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
   }
   .title {
+    text-align: center;
     font-family: var(--title-font);
+    color: #000;
+    leading-trim: both;
+    text-edge: cap;
+    font-family: BPdotsUnicase;
     font-size: 48px;
+    font-weight: 700;
+    line-height: 64px; /* 133.333% */
+    letter-spacing: -2.88px;
+    text-transform: uppercase;
+    margin-bottom: 0;
   }
   .selection {
   }
