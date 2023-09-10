fc2d731e1d937e6401fe829888379cedfa1624df
reid <reid@nativeplanet.io>
2023-09-10 18:31:16 -0500

    test segmenting logs

--- goseg/broadcast/logs.go
+++ goseg/broadcast/logs.go
@@ -35,7 +35,7 @@ func getLastLogLine(logs []byte) string {
 
 func extractTimestamp(logLine string) (time.Time, error) {
 	if len(logLine) < 19 {
-		return time.Time{}, errors.New("log line too short")
+		return time.Time{}, fmt.Errorf("log line too short")
 	}
 	layout := "2006-01-02 15:04:05"
 	timestampStr := logLine[:19]
