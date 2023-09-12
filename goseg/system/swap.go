e14c1d71c1685c0a9540f1dffa2e8c8e0d123413
reid <reid@nativeplanet.io>
2023-09-12 16:25:36 -0500

    debug swapon

--- goseg/system/swap.go
+++ goseg/system/swap.go
@@ -86,13 +86,16 @@ func activeSwap(loc string) int {
 				if strings.Contains(item, "M") || strings.Contains(item, "G") {
 					num, _ := strconv.Atoi(strings.TrimRight(item, "MG"))
 					if strings.Contains(item, "M") {
+						fmt.Println(num)
 						return num / 1024
 					}
+					fmt.Println(num)
 					return num
 				}
 			}
 		}
 	}
+	fmt.Println(0)
 	return 0
 }
 
