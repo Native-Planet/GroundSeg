59e1d3d351d18c629ac45f2cc4109dd2195b6ac3
nallux-dozryl <nallux-dozryl@protonmail.com>
2023-09-14 05:59:37 +0800

    pier settings page styling

--- ui/src/routes/[patp]/Section/Loom.svelte
+++ ui/src/routes/[patp]/Section/Loom.svelte
@@ -1,7 +1,6 @@
 <script>
+  import Slider from './Slider.svelte'
   export let loomSize
-  let curLoomSize = loomSize
-  $: adjusted = Math.log2(curLoomSize * 1024 * 1024)
 </script>
 
 <div class="section">
@@ -12,59 +11,18 @@
     </div>
   </div>
   <div class="section-right">
-    <input type="range" min="28" max="33" step="1" class="range" bind:value={adjusted}>
-    <div class="labels">
-      <div class="label danger-loom">256M</div>
-      <div class="label danger-loom">512M</div>
-      <div class="label danger-loom">1G</div>
-      <div class="label">2G</div>
-      <div class="label">4G</div>
-      <div class="label">8G</div>
+    <div class="slider">
+      <div class="spacer"></div>
+      <Slider />
     </div>
   </div>
 </div>
 
 <style>
-  .section-right {
+  .slider {
     display: flex;
-    align-items: center;
-    flex-direction: column;
-    gap: 4px;
   }
-  .range {
-    -webkit-appearance: none;
-    width: 240px;
-    height: 11px;
-    background: #ffffff4d;
-    border-radius: 16px;
-  }
-  .danger-loom {
-    color: #ffa500;
-  }
-  .range::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    appearance: none;
-    width: calc((240px / 6) - 8px);
-    height: 16px;
-    background: var(--btn-secondary);
-    cursor: pointer;
-    border-radius: 16px;
-  }
-  .range::-moz-range-thumb {
-    width: calc(240px / 6);
-    height: 16px;
-    background: var(--btn-secondary);
-    cursor: pointer;
-    border-radius: 16px;
-  }
-  .labels {
-    display: flex;
-    justify-content: space-between;
-    width: 240px;
-    padding-bottom: 6px;
-  }
-  .label {
-    width: calc(180px / 5);
-    font-size: 11px;
+  .spacer {
+    flex: 1;
   }
 </style>
