4e8d23ca4337e14e9840a272c4d6c8c881db17ec
reid <rscoggin@gmail.com>
2023-09-19 15:45:54 -0500

    fix builder

--- ui/build.sh
+++ ui/build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-docker build -t web-builder -f builder.Dockerfile
+DOCKER_BUILDKIT=0 docker build -t web-builder -f builder.Dockerfile .
 docker run --rm -v ../goseg/web:/webui/build web-builder
--- ui/builder.Dockerfile
+++ ui/builder.Dockerfile
@@ -1,4 +1,5 @@
-FROM node:18.15.0-buster-slim
+FROM node:18.17.0-buster-slim
+RUN npm install -g npm@9
 COPY ./ /webui
 WORKDIR /webui
 RUN npm install -g npm
