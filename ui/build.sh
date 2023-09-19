e1862760296eb05c774ddf374cda3ce84fbd01a8
reid <rscoggin@gmail.com>
2023-09-19 15:50:41 -0500

    fix builder

--- ui/build.sh
+++ ui/build.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 DOCKER_BUILDKIT=0 docker build -t web-builder -f builder.Dockerfile .
-docker run --rm -v web:/webui/build web-builder
+docker run --rm -v ./web:/webui/build web-builder
 mv web ../goseg/
