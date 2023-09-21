95874b0168bdb6a7baa5e12a70aa275f2be4df44
nallux-dozryl <nallux-dozryl@protonmail.com>
2023-09-22 07:14:13 +0800

    fix minio password

--- goseg/config/minio.go
+++ goseg/config/minio.go
@@ -2,13 +2,11 @@ package config
 
 import (
 	"fmt"
-	"goseg/logger"
 )
 
 func GetMinIOPassword(name string) (string, error) {
 	minioPwdMutex.Lock()
 	defer minioPwdMutex.Unlock()
-	logger.Logger.Debug(fmt.Sprintf("get - minio passwords: %+v", name))
 	password, exists := minIOPasswords[name]
 	if !exists {
 		return "", fmt.Errorf("%v password does not exist!", name)
@@ -19,7 +17,6 @@ func GetMinIOPassword(name string) (string, error) {
 func SetMinIOPassword(name, password string) error {
 	minioPwdMutex.Lock()
 	defer minioPwdMutex.Unlock()
-	logger.Logger.Debug(fmt.Sprintf("set - minio passwords: %+v", name))
 	minIOPasswords[name] = password
 	return nil
 }
