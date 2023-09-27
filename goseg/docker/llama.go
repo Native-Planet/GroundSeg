0789bd5d0cd7fed6e007b0baac6bb8c9d9322d6c
reid <rscoggin@gmail.com>
2023-09-27 11:12:51 -0500

    llama testing

--- goseg/docker/llama.go
+++ goseg/docker/llama.go
@@ -63,6 +63,7 @@ func llamaApiContainerConf() (container.Config, container.HostConfig, error) {
 				Source: "models",
 				Target: "/models",
 			},
+		},
 		CapAdd: []string{
 			"IPC_LOCK",
 		},
