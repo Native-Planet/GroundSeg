79ad52fee2a85884b16feb09f8f08902c799a409
nallux-dozryl <nallux-dozryl@protonmail.com>
2023-09-30 08:37:31 +0800

    setup ui improvements

--- ui/src/routes/setup/profile/+page.svelte
+++ ui/src/routes/setup/profile/+page.svelte
@@ -31,36 +31,75 @@
     flex: 1;
   }
   .title {
-    font-size: 48px;
+    color: #000;
+    leading-trim: both;
+    text-edge: cap;
     font-family: var(--title-font);
-    line-height: calc(48px * 2);
+    font-size: 48px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -1.92px;
+    margin-bottom: 55px;
   }
   .pw-wrapper {
-    margin-bottom: 24px;
+    margin-bottom: 32px;
   }
   .name {
-    font-size: 16px;
-    margin-bottom: 12px;
+    color: var(--NP_Black, #313933);
+    leading-trim: both;
+    text-edge: cap;
+    font-family: Inter;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+    letter-spacing: -1.44px;
+    margin-bottom: 16px;
   }
   input {
-    width: 30vw;
     min-width: 600px;
-    line-height: 42px;
+    line-height: 65px;
     border: solid 2px var(--btn-secondary);
     border-radius: 16px;
     background: none;
     padding-left: 20px;
+    color: var(--NP_Black, #313933);
+    leading-trim: both;
+    text-edge: cap;
+    font-family: Inter;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 300;
+    letter-spacing: -1.44px;
   }
   input:active, :focus {
     outline: none; 
   }
   button {
+    margin-top: 55px;
+    font-family: var(--regular-font);
     background: var(--btn-special);
     color: var(--text-card-color);
-    margin: 20px;
-    padding: 12px 32px;
-    border: none;
+    height: 65px;
+    font-size: 12px;
     border-radius: 16px;
+    cursor: pointer;
+    justify-content: center;
+    align-items: center;
+    padding: 0 48px;
+    cursor: pointer;
+
+    color: #FFF;
+    text-align: center;
+    leading-trim: both;
+    text-edge: cap;
+    font-family: Inter;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 32px; /* 133.333% */
+    letter-spacing: -1.44px;
   }
   button:hover {
     cursor: pointer;
