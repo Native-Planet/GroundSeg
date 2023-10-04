aa29045da523455ec571a207abbfe58fd7042d05
reid <rscoggin@gmail.com>
2023-10-04 17:20:23 -0500

    auth debug

--- goseg/auth/auth.go
+++ goseg/auth/auth.go
@@ -62,7 +62,7 @@ func init() {
 	conf := config.Conf()
 	authed := conf.Sessions.Authorized
 	for key := range authed {
-		ClientManager.AddAuthClient(key, &structs.MuConn{})
+		ClientManager.AddAuthClient(key, &structs.MuConn{Active: false})
 	}
 	go func() {
 		for {
