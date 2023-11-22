41ef2107b71e3af60ba832d1f7f41ca70d404d8a
nallux-dozryl <nallux-dozryl@protonmail.com>
2023-11-23 04:10:00 +0800

    add copy patp to home page

--- ui/src/routes/(home)/NameBar.svelte
+++ ui/src/routes/(home)/NameBar.svelte
@@ -1,13 +1,28 @@
 <script>
   export let patp
   export let running
+  import Clipboard from 'clipboard'
+  import { onMount } from 'svelte'
+
+  let copied = false
+  let copy
+
+  onMount(()=>{
+    copy = new Clipboard('#' + patp);
+    copy.on("success", ()=> {
+      copied = true;
+      setTimeout(()=> copied = false, 1000)
+    })
+  })
 </script>
 
 <div class="outer">
   <div class="status" class:running={running}>
     {running ? "online" : "off"}
   </div>
-  <div class="patp">{patp}</div>
+  <div class="patp" id="{patp}" data-clipboard-text={patp}>
+    {copied ? "COPIED!" : patp}
+  </div>
 </div>
 
 <style>
@@ -24,6 +39,7 @@
     text-transform: uppercase;
   }
   .patp {
+    cursor: pointer;
     color: var(--text-card-color); 
     leading-trim: both;
     text-edge: cap;
