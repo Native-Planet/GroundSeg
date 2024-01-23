3657a5d2256e501bd528fba4be9e5aff52fa2b5b
nallux-dozryl <nallux-dozryl@protonmail.com>
2024-01-23 08:32:14 +0800

    add startram satellite button to navbar

--- ui/src/lib/Nav.svelte
+++ ui/src/lib/Nav.svelte
@@ -3,6 +3,7 @@
   import { page } from '$app/stores'
   import { wide, version } from '$lib/stores/display'
   import { structure, URBIT_MODE } from '$lib/stores/data'
+  import SatelliteDish from '$lib/SatelliteDish.svelte'
 
   // Temp dev mode
   import DevToggle from '$lib/DevToggle.svelte'
@@ -25,28 +26,33 @@
 </script>
 
 <div class="wrapper {wide ? "wide" : "slim"}">
-  <div class="app">
-    GROUNDSEG {$version} - STARTRAM {
-      !registered ? "UNREGISTERED" :
-      running ? "ONLINE" : "OFFLINE"
-      }
-      <!--
-    <DevToggle />
-      -->
-  </div>
   {#if ($page.route.id == '/[patp]') || ($page.route.id.includes('/boot'))}
     {#if authLevel == "authorized"}
       <div class="back" on:click={handleBack}></div>
     {/if}
   {:else}
     <div class="nav">
-      <div class="ships">
-        <div
-          class:highlight={$page.route.id != (pfx+"/(home)")}
-          on:click={()=>goto(pfx+"/")}
-          class="btn option"
-          >SHIPS
+      <div class="status-bar">
+        <div class="app">
+          <div class="app-name">GROUNDSEG</div>
+          <div class="app-version">{$version}</div>
         </div>
+        <SatelliteDish />
+        <!--
+        - STARTRAM {
+          !registered ? "UNREGISTERED" :
+          running ? "ONLINE" : "OFFLINE"
+          }
+          <!--
+        <DevToggle />
+          -->
+      </div>
+      <div class="spacer"></div>
+      <div
+        class:highlight={$page.route.id != (pfx+"/(home)")}
+        on:click={()=>goto(pfx+"/")}
+        class="btn option"
+        >SHIPS
       </div>
       <div
         class:highlight={$page.route.id != (pfx+"/apps")}
@@ -81,21 +87,40 @@
   .wrapper {
     color: var(--text-color);
     margin: auto;
-    margin-top: 10px;
-  }
-  .version {
-    font-family: var(--title-font);
-    font-size: 14px;
-  }
-  .app {
-    font-family: var(--title-font);
-    font-size: 16px;
-    margin-bottom: 16px;
   }
   .nav {
     display: flex;
-    margin-bottom: 10px;
+    margin-top: 4px;
     gap: 24px;
+    align-items: end;
+    margin-bottom: 16px;
+  }
+  .spacer {
+    flex: 1;
+  }
+  .status-bar {
+    display: flex;
+    gap: 16px;
+    align-items: end;
+  }
+  .app {
+    position: relative;
+    height: 32px;
+    width: 180px;
+  }
+  .app-name {
+    position: absolute;
+    font-family: var(--title-font);
+    font-size: 32px;
+    top: -15px;
+    left: 0;
+  }
+  .app-version {
+    position: absolute;
+    font-family: var(--title-font);
+    font-size: 14px;
+    bottom: 0;
+    left: 0;
   }
   .back {
     width: 48px;
