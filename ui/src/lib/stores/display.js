970c33ec93962d9bd52cded699b91f682f4a7ba2
reid <yapishu@users.noreply.github.com>
2025-04-05 01:50:20 -0500

    match env var

--- ui/src/lib/stores/display.js
+++ ui/src/lib/stores/display.js
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store'
   
 export const wide = writable(true)
-export const version = writable(import.meta.env.VITE_APP_VERSION || "dev")
+export const version = writable(import.meta.env.GS_VERSION || "dev")
